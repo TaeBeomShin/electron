34d9983d97e6b85909afaf9e2d91bbed0d9e4292
annie <a@hinaria.com>
2020-06-15 13:05:30 -0700

    osr: send frame contents via `discord_overlay2.node`.

--- shell/browser/api/electron_api_web_contents.cc
+++ shell/browser/api/electron_api_web_contents.cc
@@ -2526,7 +2526,10 @@ bool WebContents::IsOffScreen() const {
 
 #if BUILDFLAG(ENABLE_OSR)
 void WebContents::OnPaint(const gfx::Rect& dirty_rect, const SkBitmap& bitmap) {
-  Emit("paint", dirty_rect, gfx::Image::CreateFrom1xBitmap(bitmap));
+  if (!overlay_.SendFrame(bitmap.width(), bitmap.height(), bitmap.getPixels(),
+                          bitmap.rowBytes() * bitmap.height())) {
+    Emit("paint", dirty_rect, gfx::Image::CreateFrom1xBitmap(bitmap));
+  }
 }
 
 void WebContents::StartPainting() {
@@ -2884,6 +2887,8 @@ void WebContents::BuildPrototype(v8::Isolate* isolate,
   prototype->SetClassName(gin::StringToV8(isolate, "WebContents"));
   gin_helper::Destroyable::MakeDestroyable(isolate, prototype);
   gin_helper::ObjectTemplateBuilder(isolate, prototype->PrototypeTemplate())
+      .SetMethod("_setDiscordOverlayProcessId",
+                 &WebContents::SetDiscordOverlayProcessID)
       .SetMethod("getBackgroundThrottling",
                  &WebContents::GetBackgroundThrottling)
       .SetMethod("setBackgroundThrottling",
@@ -3035,6 +3040,10 @@ gin::Handle<WebContents> WebContents::FromOrCreate(
     return existing;
   else
     return gin::CreateHandle(isolate, new WebContents(isolate, web_contents));
+}
+
+void WebContents::SetDiscordOverlayProcessID(uint32_t process_id) {
+  overlay_.SetProcessId(process_id);
 }
 
 }  // namespace api
--- shell/browser/api/electron_api_web_contents.h
+++ shell/browser/api/electron_api_web_contents.h
@@ -50,6 +50,8 @@ class ScriptExecutor;
 }
 #endif
 
+#include "discord/overlay.h"
+
 namespace blink {
 struct WebDeviceEmulationParams;
 }
@@ -134,6 +136,12 @@ class WebContents : public gin_helper::TrackableObject<WebContents>,
                     public CommonWebContentsDelegate,
                     public content::WebContentsObserver,
                     public mojom::ElectronBrowser {
+ public:
+  void SetDiscordOverlayProcessID(uint32_t process_id);
+
+ private:
+  discord::Overlay overlay_ = {};
+
  public:
   enum class Type {
     BACKGROUND_PAGE,  // A DevTools extension background page.
